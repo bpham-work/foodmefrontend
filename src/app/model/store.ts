61a4907765c6776d900dc46ebd2cce87cc84fcdf
Bryant Pham <bryantpham@Bryants-MacBook-Pro.local>
2018-05-20 01:02:33 -0500

    Add rating to store

--- src/app/model/store.ts
+++ src/app/model/store.ts
@@ -3,15 +3,17 @@ export class Store {
   public name: string;
   public lat: number;
   public lng: number;
+  public rating: number;
 
   public static from(json: any) {
-    return new Store(json.id, json.name, json.geometry.location.lat, json.geometry.location.lng);
+    return new Store(json.id, json.name, json.geometry.location.lat, json.geometry.location.lng, json.rating);
   }
 
-  constructor(id: string, name: string, lat: number, lng: number) {
+  constructor(id: string, name: string, lat: number, lng: number, rating: number) {
     this.id = id;
     this.name = name;
     this.lat = lat;
     this.lng = lng;
+    this.rating = rating;
   }
 }
