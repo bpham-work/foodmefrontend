0c5e581c98945fec9e02e52646d4d8876bcc583f
Bryant Pham <bryantpham@Bryants-MacBook-Pro.local>
2018-05-20 05:16:03 -0500

    Add map shifting

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -10,11 +10,28 @@ import { GroceryStore } from '../model/grocerystore';
 })
 export class MapComponent implements OnInit {
   public stores: GroceryStore[] = [];
+  public cameraLat: number;
+  public cameraLng: number;
+  public zoom: number = 12;
 
   constructor(private storeService: StoreService) {}
 
   public ngOnInit(): void {
     this.storeService.getStores()
-      .subscribe((stores: GroceryStore[]) => this.stores = stores);
+      .subscribe((stores: GroceryStore[]) => {
+        this.stores = stores;
+        if (stores.length > 0) {
+          this.cameraLat = this.stores[0].lat;
+          this.cameraLng = this.stores[0].lng;
+          this.zoom = 12;
+        }
+      });
+
+    this.storeService.getSelectedStore()
+      .skipWhile((n: any) => !n)
+      .subscribe((store: GroceryStore) => {
+        this.cameraLat = store.lat;
+        this.cameraLng = store.lng;
+      });
   }
 }
